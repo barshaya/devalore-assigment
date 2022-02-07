b8bf4186e4ea9d04bfa68dc3dd5cf8f295fceff1
barshaya <barshaya97@gmail.com>
2022-02-07 21:05:50 +0200

    add total age func

--- client/src/Pages/Pets/Pets.js
+++ client/src/Pages/Pets/Pets.js
@@ -1,33 +1,52 @@
 import React, { useEffect, useState } from "react";
 import Pet from "../../components/PetCard";
 import { getPets } from "../../services/petServices";
-import './Pets.css'
+import "./Pets.css";
 
 function Pets() {
   const [pets, setPets] = useState([]);
   const [total, setTotal] = useState(0);
+  const [totalAge, setTotalAge] = useState(0);
+
+  const fetchData = async () => {
+    try {
+      await getPets().then((res) => {
+        const data = res.data;
+        setPets(data);
+        console.log("change");
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   useEffect(() => {
-    getPets().then((res) => {
-      const data = res.data;
-      setPets(data);
-      console.log('change')
-    });
+    fetchData();
   }, []);
 
-
   useEffect(() => {
-  setTotal(pets.length);
-  }, [pets]);
+    if(pets.length>0){
+    setTotal(pets.length);
+    
+    let sum = pets.reduce((prev, cur) => prev + (cur.age || 0), 0);
+    setTotalAge(sum)
+    }
+  }
+  , [pets]);
 
   return (
-    <>
-      {pets&& <p className="pets-total">total : {total}</p>}    
+    <div className="pets-container">
+      {pets && (
+        <>
+          <h2>total : {total}</h2>
+          <h2>totalAge : {totalAge}</h2>
+        </>
+      )}
       {pets &&
         pets.map((pet) => {
-          return <Pet key={pet._id} pet={pet}/>;
+          return <Pet key={pet._id} pet={pet} />;
         })}
-    </>
+    </div>
   );
 }
 
